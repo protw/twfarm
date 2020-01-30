65716324b56215fac6cb4880a3b1ce1bbf629301
oleghbond <o.o.bondarenko@gmail.com>
2020-01-30 23:01:17 +0200

    Update of Thu Jan 30 2020 23:01:17 GMT+0200 (GMT+02:00)

--- tw_builder.bat
+++ tw_builder.bat
@@ -3,3 +3,4 @@ REM octave-cli tw_builder.m
 
 REM TW Farm Builder Script version 2
 node tw_builder_2.js
+copy /y D:\boa_protw\wiki_bag\prosteer\issuetracker.html D:\boa_protw\wiki_bag\prosteer\docs\
