9153c55e54e8c67552d8719e97049a5b55f58785
oleghbond <o.o.bondarenko@gmail.com>
2020-06-07 19:17:08 +0300

    Update of Sun Jun 07 2020 19:17:08 GMT+0300 (GMT+03:00)

--- tw_builder.js
+++ tw_builder.js
@@ -116,11 +116,14 @@ function tw_html_builder(w_name) {
 	let img_filter = `"[is[image]] -[prefix[$:/]]"`;
 
 	let html_tw_build_cmds = [
-		//// delete all files recursively inside directory ${html_images_dir}
+		/* delete all files recursively inside directory ${html_images_dir}
+		 * delete directory ${html_images_dir}
+		 * delete file ${html_index_file}
+		 * copy all wiki files recursively from ${tw_dir} to ${tmp_dir} for canonical externalization
+		**/
 		`del /Q "${html_images_dir}\\*.*"`,
-		//// delete file ${html_index_file}
+		`del /Q "${html_images_dir}"`,
 		`del /Q "${html_index_file}"`,
-		//// copy all wiki files recursively from ${tw_dir} to ${tmp_dir} for canonical externalization
 		`xcopy /s/i/q "${tw_dir}\\*.*" "${tmp_dir}" /exclude:tw_exclude.list`,
 		//// canonical externalization all selected images ${img_filter} of wiki in directory ${tmp_dir}
 		//// and generating the result in ${html_index_file} and ${html_images_dir}
